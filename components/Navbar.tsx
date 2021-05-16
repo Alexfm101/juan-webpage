51e1b07d92d6daf6a1808f39d552b136fa60ce73
alex <cuatroalejandro@gmail.com>
2021-05-15 23:29:14 -0400

    add navbar component and new pages

---
+++ components/Navbar.tsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import Link from "next/link";
+
+export default function Navbar() {
+    const [isOpen, setIsOpen ] = useState(false);
+
+    return (
+        <>
+            <div className="max-w-screen-xl mx-auto ">
+                <nav className=" w-full md:w-auto p-2 md:mx-2">
+                    <div className="flex justify-between">
+                        <img className={` h-10 ${ isOpen ? 'invisible' : ''} `} src="/svgs/logo.svg" alt="" />
+                        <button className="focus:outline-none md:hidden" onClick={() => setIsOpen(!isOpen)}>
+                            <img className={`  ${ isOpen ? 'hidden' : ''}`} src='/svgs/menu.svg' alt="" />
+                            <img className={`  ${ isOpen ? '' : 'hidden'}`} src='/svgs/close.svg' alt="" />
+                        </button>
+                        <div className="hidden md:flex">
+                            <ul className="md:flex md:flex-row md:my-auto md:space-x-5">
+                                <li>
+                                    <Link href="/Portafolio">
+                                        <a className="font-bold text-gray-800" href="">Portafolio</a>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href="/Bibliografia">
+                                        <a className="font-bold text-gray-800" href="">Bibliografia</a>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href="/Contactame">
+                                        <a className="font-bold text-gray-800" href="">Contactame</a>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className={`flex justify-center md:hidden ${isOpen ? '' : 'hidden'}`}>
+                        <ul className="space-y-2">
+                            <li>
+                                <Link href="/Portafolio">
+                                    <a className="font-bold text-gray-800" href="">Portafolio</a>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/Bibliografia">
+                                    <a className="font-bold text-gray-800" href="">Bibliografia</a>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/Contactame">
+                                    <a className="font-bold text-gray-800" href="">Contactame</a>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </>
+    );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,9 @@
+import  Navbar  from "../components/Navbar";
+
 export default function Home() {
-	return <div className="bg-blue-400 flex justify-center">hola juan</div>;
+	return (
+		<div className="">
+			<Navbar/>
+		</div>
+	);
 }
