9d9f278a0b082b83647d082dc158c9e4fd8cb7cd
alex <cuatroalejandro@gmail.com>
2021-05-27 23:20:23 -0400

    update contactame

--- pages/Contactame.tsx
+++ pages/Contactame.tsx
@@ -4,7 +4,50 @@ export default function Contactame() {
 	return (
 		<>
 			<Navbar />
-			Contactame
+			<section className="mt-10">
+				<div className="flex justify-center flex-col space-y-10">
+					<img src="/svgs/logo.svg" className=" h-24 md:h-32 lg:h-40" alt="" />
+					<h1 className="text-center font-Comfortaa font-bold text-gray-700 text-3xl">Contactame</h1>
+				</div>
+			</section>
+
+			{/* formulario */}
+			<section className="mt-10 md:max-w-lg md:flex md:mx-auto">
+				<form className="p-4 space-y-5">
+					<div className="space-y-5 md:flex md:flex-row md:space-y-0 md:space-x-2">
+						<input 
+							className="border-2 w-full p-2 border-gray-700 rounded-lg focus:outline-none focus:border-red-400 shadow-xl" 
+							type="text" 
+							placeholder="Nombre" 
+							name="email" 
+							id="name" 
+						/>
+						<input 
+							className="border-2 w-full p-2 border-gray-700 rounded-lg focus:outline-none focus:border-red-400 shadow-xl" 
+							type="email"  
+							placeholder="correo" 
+							name="email" 
+							id="email" 
+						/>
+					</div>
+					<textarea 
+						className="border-2 w-full p-2 border-gray-700 rounded-lg focus:outline-none focus:border-red-400 shadow-2xl" 
+						name="" 
+						id="" 
+						cols={30} 
+						rows={15}
+						placeholder="En que te puedo ayudar"
+					/>
+					<div className="flex justify-center">
+						<button 
+							className=" bg-gray-700 px-4 py-1 rounded-xl text-xl text-white font-Comfortaa font-bold focus:outline-none focus:bg-red-400" 
+							type="submit"
+						>
+							Enviar
+						</button>
+					</div>
+				</form>
+			</section>
 		</>
 	);
 }
