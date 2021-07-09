c9cdf95d6ccbd54aa9970791465661a15266f766
alex <cuatroalejandro@gmail.com>
2021-07-08 20:41:36 -0400

    make footer responsive

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,27 +1,36 @@
 import Link from 'next/link';
 
 export default function Footer() {
+	const currentYear = new Date().getFullYear();  
+
 	return (
 		<>
-			<footer className="flex flex-wrap space-x-10 w-full justify-center items-center p-4">
-				<Link href="#">
-					<a href="">
-						<img
-							className="w-auto h-10"
-							src="/svgs/instagram.svg"
-							alt=""
-						/>
-					</a>
-				</Link>
-				<Link href="#">
-					<a href="">
-						<img
-							className="w-auto h-10"
-							src="/svgs/begance.svg"
-							alt=""
-						/>
-					</a>
-				</Link>
+			<footer className="w-full p-4">
+				<div className="flex flex-row justify-center space-x-10">
+					<Link href="#">
+						<a href="">
+							<img
+								className="w-auto h-8 lg:h-10"
+								src="/svgs/instagram.svg"
+								alt=""
+							/>
+						</a>
+					</Link>
+					<Link href="#">
+						<a href="">
+							<img
+								className="w-auto h-8 lg:h-10"
+								src="/svgs/begance.svg"
+								alt=""
+							/>
+						</a>
+					</Link>
+				</div>
+				<div className="text-center mt-2 lg:text-lg">
+                	<small>
+						&copy; {currentYear} All rights reserved, Fraum
+					</small> 
+            	</div>
 			</footer>
 		</>
 	);
