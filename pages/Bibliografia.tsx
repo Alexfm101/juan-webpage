4d3de33b935a682e65c38469c803a97927922720
alex <cuatroalejandro@gmail.com>
2021-08-20 20:13:58 -0400

    update bibliografia style

--- pages/Bibliografia.tsx
+++ pages/Bibliografia.tsx
@@ -6,28 +6,26 @@ export default function Bibliografia() {
 			<Navbar />
 			<section className="mt-10">
 				<div className="flex justify-center">
-					<h1 className="text-center font-Comfortaa font-bold text-gray-700 text-3xl">
+					<h1 className="text-center font-Comfortaa font-bold text-gray-800 text-3xl">
 						Bibliografia
 					</h1>
 				</div>
 			</section>
 			<section className="mt-10 flex flex-col md:flex-row md: max-w-2xl md:mx-auto">
-				<div className="p-4 flex flex-col space-y-2 ">
-					<p className="text-center font-Comfortaa text-lg font-medium text-gray-900">
-						Hey! mi nombre es Juan Fraudita.
-					</p>
-					<p className="text-center font-Comfortaa text-lg font-medium text-gray-900">
-						Soy fotografo y artista digital.
-					</p>
-					<p className="text-center font-Comfortaa text-lg font-medium text-gray-900">
-						Vivo en Venezuela
-					</p>
-					<p className="text-center font-Comfortaa text-lg font-medium text-gray-900">
-						Mientras estudio una licenciatura en artes me dedico al
-						arte digital y fotografiar momentos iconicos dentro de
-						la fotografia de moda y retratos.
+				<div className="shadow-lg mt-10 p-2 max-w-2xl mx-auto">
+					<div className="flex flex-col items-center text-center px-4 mt-5 font-semibold text-gray-500 xl:text-xl">
+						<p className="font-Comfortaa">Hey! mi nombre es Juan.</p>
+						<p className="font-Comfortaa">
+							Soy Fotografo y Artista Digital.
+						</p>
+						<p className="font-Comfortaa">Vivo en Venezuela.</p>
+						<p className="font-Comfortaa">
+							Mientras estudio una licenciatura en artes, me dedico al
+							arte digital y fotografiar momentos iconicos dentro de
+							la fotografia de moda y retratos.
 					</p>
 				</div>
+			</div>
 				<div className="">
 					<div className="w-full">
 						<div className="relative h-72 w-72 flex justify-center mx-auto">
