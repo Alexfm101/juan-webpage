0b5efe7b0086b5d5081bbb34137d4f69953bc702
alex <cuatroalejandro@gmail.com>
2021-09-16 10:38:04 -0400

    update index and images portofilio

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,42 +7,8 @@ export default function Home() {
 
 			<ul className="container md:grid md:grid-cols-3 lg:grid-cols-4 gap-2 md:mx-auto ">
 				<li className="">
-					<img className="" src="/images/Portafolio/_MG_0050.jpg" alt="" />
+					<img className="" src="/images/portafolio/_MG_0050.jpg" alt="" />
 				</li>
-				<li className="">
-					<img className="" src="/images/Portafolio/_MG_0051.jpg" alt="" />
-				</li>
-				<li className="">
-					<img className="" src="/images/Portafolio/_MG_0063.jpg" alt="" />
-				</li>
-				<li className="">
-					<img className="" src="/images/Portafolio/_MG_0067.jpg" alt="" />
-				</li>
-				<li className="">
-					<img className="" src="/images/Portafolio/_MG_0107.jpg" alt="" />
-				</li>
-				<li className="">
-					<img src="/images/Portafolio/_MG_0113.jpg" alt="" />
-				</li>
-				<li className="">
-					<img src="/images/Portafolio/_MG_0321.jpg" alt="" />
-				</li>
-				<li className="">
-					<img src="/images/Portafolio/_MG_0371.jpg" alt="" />
-				</li>
-				<li className=" ">
-					<img src="/images/Portafolio/_MG_0386.jpg" alt="" />
-				</li>
-				<li className=" ">
-					<img src="/images/Portafolio/_MG_0764.jpg" alt="" />
-				</li>
-				<li className=" ">
-					<img src="/images/Portafolio/_MG_0800.jpg" alt="" />
-				</li>
-				<li className="">
-					<img src="/images/Portafolio/_MG_0847.jpg" alt="" />
-				</li>
-
 			</ul>
 	
 		</>
