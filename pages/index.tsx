5c48874e9df76175b4c473ce2f0a22f50f22d472
alex <cuatroalejandro@gmail.com>
2021-09-16 11:26:17 -0400

    more updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,29 @@
 import Navbar from '../components/Navbar';
+import {getImages} from '../lib/images';
 
-export default function Home() {
+export default function Home({Images}) {
+	
 	return (
 		<>
 			<Navbar/>
 
 			<ul className="container md:grid md:grid-cols-3 lg:grid-cols-4 gap-2 md:mx-auto ">
-				<li className="">
-					<img className="" src="/images/portafolio/_MG_0050.jpg" alt="" />
-				</li>
+				{Images.map((id) => (
+					<li key={id}>
+						<img className="" src={`images/portfolio/${id}.jpg`} alt="" />
+					</li>
+				))}
 			</ul>
 	
 		</>
 	);
 }
+
+export async function getStaticProps() {
+	const Images = getImages()
+	return {
+		props: {
+			Images
+		}
+	}
+};
