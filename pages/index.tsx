873baa2a770b96f2c859cdcab9b0e90c579d75ff
alex <cuatroalejandro@gmail.com>
2022-01-21 22:40:49 -0400

    fix id of list

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,8 +16,8 @@ export default function Home({ pictures }: IGallery) {
 			<Navbar/>
 
 			<ul className="container md:mx-auto md:masonry">
-				{pictures.map((picture, idx) => (
-					<li key={idx} className="m-2 md:m-0 md:item">
+				{pictures.map((picture, id) => (
+					<li key={id} className="m-2 md:m-0 md:item">
 						<img className="" src={picture.large} alt="" />
 					</li>
 				))}
