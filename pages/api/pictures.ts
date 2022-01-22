a79859af3db8b69fd577d99b19f5921c4e0d5d8e
alex <cuatroalejandro@gmail.com>
2022-01-21 22:46:56 -0400

    add secure https mode in cloudinary

--- pages/api/pictures.ts
+++ pages/api/pictures.ts
@@ -3,7 +3,8 @@ import cloudinary from 'cloudinary';
 cloudinary.v2.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET
+    api_secret: process.env.API_SECRET,
+    secure: true,
 });
 
 export const getPictures = async (tag: string) => {
