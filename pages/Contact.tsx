8dfca9ea59a85d154f423381eb282c498d19e9b0
alex <cuatroalejandro@gmail.com>
2022-01-23 15:14:00 -0400

    add contact form feature

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -14,7 +14,15 @@ export default function Contactame() {
 
 			{/* formulario */}
 			<section className="mt-10 md:max-w-xl md:flex md:mx-auto">
-				<form className="p-4 space-y-5">
+				<form 
+					className="p-4 space-y-5" 
+					name="contact" 
+					method="POST" 
+					data-netlify="true" 
+					action="/Sucess"
+					netlify-honeypot="bot-field"
+					data-netlify-recaptcha="true"
+				>
 					<div className="space-y-5 md:flex md:flex-row md:space-y-0 md:space-x-2">
 						<input
 							className="border-2 w-full p-2 border-gray-700 rounded-lg focus:outline-none focus:border-red-400 shadow-xl"
@@ -39,6 +47,12 @@ export default function Contactame() {
 						rows={15}
 						placeholder="En que te puedo ayudar"
 					/>
+					<label className="hidden">
+						Dont't fill this out if you're human:
+						<input name="bot-field"/>
+					</label>
+					<div data-netilfy-recaptcha="true"></div>
+					<input type="hidden" name="form-name"  value="contact"/>
 					<div className="flex justify-center">
 						<button
 							className=" bg-gray-800 px-6 py-2 rounded-xl text-xl text-white font-Comfortaa font-bold focus:outline-none focus:bg-red-400"
