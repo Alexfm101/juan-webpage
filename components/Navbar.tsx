eec82a08faf6e4b367a8c3bc3f1b406034ece412
alex <cuatroalejandro@gmail.com>
2022-04-15 00:52:11 -0400

    more updates

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,24 +1,20 @@
 import { useState } from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
 
 export default function Navbar() {
 	const [isOpen, setIsOpen] = useState(false);
-	const [portfolio, setPortfolio] = useState(false);
 
 	return (
 		<>
 			<header className=" max-w-screen-xl mx-auto">
-				<nav className="w-full px-4 py-7 ">
+				<nav className="w-full px-2 py-7 lg:px-0">
 					<div className="flex justify-between">
 						<Link href="/">
-							<a href="" className="relative w-14 h-10 md:h-12 lg:16 " >
-								<Image
-									className={`${
-										isOpen ? 'invisible md:visible' : ''
-									} `}
+							<a href="" className="relative w-12 lg:w-16" >
+								<img
+									className={`${isOpen ? 'invisible md:visible' : ''
+										} `}
 									src="/svgs/logo.svg"
-									layout="fill"
 								/>
 							</a>
 						</Link>
@@ -34,8 +30,8 @@ export default function Navbar() {
 								width="24px"
 								fill="#202536"
 							>
-							    <path d="M0 0h24v24H0z" fill="none"/>
-							    <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
+								<path d="M0 0h24v24H0z" fill="none" />
+								<path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
 							</svg>
 
 							<svg
@@ -46,8 +42,8 @@ export default function Navbar() {
 								width="24px"
 								fill="#202536"
 							>
-							    <path d="M0 0h24v24H0z" fill="none"/>
-							    <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+								<path d="M0 0h24v24H0z" fill="none" />
+								<path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
 							</svg>
 
 						</button>
@@ -55,40 +51,6 @@ export default function Navbar() {
 						{/* desktop style */}
 						<div className="hidden md:flex">
 							<ul className="md:flex md:flex-row md:my-auto md:space-x-5">
-								{/* <li>
-									<button
-										className=" text-gray-900  lg:text-lg font-medium font-Confortaa hover:text-red-400 focus:outline-none"
-										onClick={() => setPortfolio(!portfolio)}
-									>
-										Portafolio
-									</button>
-									<ul className={`${ portfolio? '' : 'hidden'}`}>
-										<li> 
-											<Link href="/portfolio/Test1">
-												<a href="" className='text-gray-600  lg:text-base 
-													font-Confortaa hover:text-indigo-600'>
-													test1
-												</a>
-											</Link>
-										</li>
-										<li><
-											Link href="/portfolio/Test2">
-												<a href="" className='text-gray-600  lg:text-medium 
-													font-Confortaa hover:text-indigo-600'>
-														test2
-												</a>
-											</Link>
-										</li>
-										<li>
-											<Link href="/portfolio/Test3">
-												<a href="" className='text-gray-600  lg:text-medium 
-													font-Confortaa hover:text-indigo-600'>
-													test 3
-												</a>
-											</Link>
-										</li>
-									</ul>
-								</li> */}
 								<li>
 									<Link href="/Me">
 										<a
@@ -115,45 +77,10 @@ export default function Navbar() {
 
 					{/* mobile style */}
 					<div
-						className={`flex justify-center text-center md:hidden px-4 py-5 rounded-sm border bg-gray-50 shadow-lg  ${
-							isOpen ? '' : 'hidden'
-						}`}
+						className={`flex justify-center text-center md:hidden px-4 py-5 rounded-sm border bg-gray-50 shadow-lg  ${isOpen ? '' : 'hidden'
+							}`}
 					>
 						<ul className="space-y-3">
-							{/* <li>
-								<button
-									className=" text-gray-900  text-lg font-medium font-Confortaa hover:text-red-400 focus:outline-none"
-									onClick={() => setPortfolio(!portfolio)}
-								>
-									Portafolio
-								</button>
-								<ul className={`${ portfolio? '' : 'hidden'}`}>
-									<li> 
-										<Link href="/portfolio/Test1">
-											<a href="" className='text-gray-600  lg:text-base 
-												font-Confortaa hover:text-indigo-600'>
-												test1
-											</a>
-										</Link>
-									</li>
-									<li><
-										Link href="/portfolio/Test2">
-											<a href="" className='text-gray-600  lg:text-medium 
-												font-Confortaa hover:text-indigo-600'>
-													test2
-											</a>
-										</Link>
-									</li>
-									<li>
-										<Link href="/portfolio/Test3">
-											<a href="" className='text-gray-600  lg:text-medium 
-												font-Confortaa hover:text-indigo-600'>
-												test 3
-											</a>
-										</Link>
-									</li>
-								</ul>
-							</li> */}
 							<li>
 								<Link href="/Me">
 									<a
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,23 @@ import Navbar from '../components/Navbar';
 import { getPictures } from './api/pictures';
 import { AdvancedImage } from '@cloudinary/react';
 
+
+
 export default function Home() {
 
-	const pictures = getPictures("home", 20);
+  	const [pictures, setPictures] = React.useState([]);
 	const [open, setOpen] = React.useState(false);
 	const [picture, setPicture] = React.useState();
+
+	React.useEffect(() => {
+		const fetchdata = async () => {
+			const data = await getPictures("home", 20);
+			setPictures(data);
+		}
+
+		fetchdata().catch(console.error);
+	}, []);
+ 
 
 	const handleOnClick = (e) => {
 		setPicture(e.target.src);
@@ -20,9 +32,9 @@ export default function Home() {
 			<Head>
 				<title>Home | Fraum</title>
 				<meta name="description" property="og:description" content="Juan Fraudita - Fotografía" />
-				<meta property="og:title" content="Home | Fraum"/>
-				<meta property="og:url" content="https://fraum.vercel.app/"/>
-				<meta property="og:type" content="website"/>
+				<meta property="og:title" content="Home | Fraum" />
+				<meta property="og:url" content="https://fraum.vercel.app/" />
+				<meta property="og:type" content="website" />
 			</Head>
 
 			<div className='relative overflow-hidden'>
@@ -37,18 +49,18 @@ export default function Home() {
 				{open && (
 					<div className="fixed z-10 inset-0 overflow-y-auto bg-black bg-opacity-80">
 						<button onClick={() => setOpen(false)} className='p-4 focus:outline-none' >
-								<svg
-									className=""
-									xmlns="http://www.w3.org/2000/svg"
-									height="24px"
-									viewBox="0 0 24 24"
-									width="24px"
+							<svg
+								className=""
+								xmlns="http://www.w3.org/2000/svg"
+								height="24px"
+								viewBox="0 0 24 24"
+								width="24px"
 
-									fill="#FFFFFF"
-								>
-									<path d="M0 0h24v24H0z" fill="none"/>
-									<path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
-								</svg>	
+								fill="#FFFFFF"
+							>
+								<path d="M0 0h24v24H0z" fill="none" />
+								<path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+							</svg>
 						</button>
 
 						<img src={picture} className="relative px-5 md:py-5 md:px-60" alt="portfolio" />
