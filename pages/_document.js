03a47e0da668ef6024a2a58d8f0ef0bfe7e1e4d9
alex <cuatroalejandro@gmail.com>
2022-04-15 03:51:59 -0400

    fix logo

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,6 @@ export default function Document() {
                     rel="stylesheet"
                 />
                 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet"></link>
-                <link rel="icon" type="image/svg" href="/svgs/logo.svg"/>
             </Head>
             <body>
                 <Main/>
