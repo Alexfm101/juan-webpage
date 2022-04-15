53df3a2bad7b507eded91c9f2f44b2d1ea4eccee
alex <cuatroalejandro@gmail.com>
2022-04-14 23:01:12 -0400

    add error pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,14 +6,6 @@ import '../styles/globals.css';
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
-			<head>
-				<link rel="preconnect" href="https://fonts.gstatic.com" />
-				<link
-					href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap"
-					rel="stylesheet"
-				/>
-				<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet"></link>
-			</head>
 			<Component {...pageProps} />
 			<Footer />
 		</>
